7a7cf0da6e3cffd2b87c6c318c6e85f35877ee6b
Denis Zakharchuk <albertgutenberg1@gmail.com>
2025-11-02 20:42:27 +0300

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 
+const isCI = process.env.GITHUB_ACTIONS === "true";
+const repository = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+
 export default defineConfig({
+  base: isCI && repository ? `/${repository}/` : "/",
   build: {
     rollupOptions: {
       input: {

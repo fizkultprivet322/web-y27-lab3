39616e5e7076f5c6fb1ede3df3d34afe38079300
Denis Zakharchuk <albertgutenberg1@gmail.com>
2025-11-02 20:50:10 +0300

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "./" : "/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "./" : "/",
   build: {
     rollupOptions: {
       input: {
@@ -12,4 +12,4 @@ export default defineConfig({
       }
     }
   }
-});
+}));
